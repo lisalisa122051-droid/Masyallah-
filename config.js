b68868837cd9d3aa6faf8d99e6653916b0d6ead9
lisalisa122051-droid <lisalisa122051@gmail.com>
2025-12-28 07:06:22 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * config.js
+ * Basic configuration and owner definition
+ */
+
+module.exports = {
+  ownerNumber: '6281234567890', // change to your number in normalized format (without @s.whatsapp.net)
+  prefix: '.',
+  sessionPath: './session',
+  databaseFile: './database.json',
+  botName: 'BotWA-MD',
+  version: '1.0.0',
+  welcomeMessage: 'Selamat datang di grup!',
+  goodbyeMessage: 'Sampai jumpa!',
+  antiLink: {
+    enabled: false,
+    kick: false
+  }
+};
